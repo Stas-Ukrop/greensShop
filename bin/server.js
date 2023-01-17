1b1905955d53176b0f76714ed49b005c83d9eb62
Stas-Ukrop <48555868+Stas-Ukrop@users.noreply.github.com>
2023-01-17 17:34:24 +0100

    Add files via upload
    correct the db

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,14 @@
 import api from '../components/api/api.js'
+import myDB from '../components/db/db.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 
-api.listen(PORT, () => {
-    console.log(`${PORT}`)
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+myDB.then(() => {
+    api.listen(PORT, (req, res) => {
+        console.log(PORT)
+    })
+}).catch(err => {
+    next(err)
+})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
