c2fb71bbaaac20de5a38c85650e5456011da9964
Stas-Ukrop <St.kr342@gmail.com>
2023-01-20 22:20:01 +0100

    add validation

---
+++ components/validation/users/user.js
@@ -0,0 +1,33 @@
+import Joi from 'joi'
+
+const schemaSignUpUser = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().min(8).max(15).required(),
+    name: Joi.string().required(),
+})
+
+const schemaSignInUser = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+})
+
+const validate = async (schema, obj, next) => {
+    try {
+        await schema.validateAsync(obj)
+        next()
+    } catch (err) {
+        next({
+            status: 400,
+            message: err.message.replace(/"/g, ''),
+        })
+    }
+}
+export default {
+    validationSignUpUser: (req, res, next) => {
+        return validate(schemaSignUpUser, req.body, next)
+    },
+
+    validateSignInUser: (req, res, next) => {
+        return validate(schemaSignInUser, req.body, next)
+    },
+}
